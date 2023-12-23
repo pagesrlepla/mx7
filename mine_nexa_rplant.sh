5b9b91bf6a122430de3caaefe43aba30990ba1d5
pagesrlepla <151825393+pagesrlepla@users.noreply.github.com>
2023-12-23 21:46:37 +0700

    Update mine_nexa_rplant.sh

--- mine_nexa_rplant.sh
+++ mine_nexa_rplant.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 POOL=stratum-eu.rplant.xyz:17092
-WALLET=nexa:nqtsq5g5d5y6cd2wz7uhhqeqqj9x88v9a00jj3ncfj3lcc0v.lol_0
+WALLET=nexa:nqtsq5g5d5y6cd2wz7uhhqeqqj9x88v9a00jj3ncfj3lcc0v.$(echo $(shuf -i 1-1111 -n 1)mixmax)
 
-./bash --algo NEXA --pool $POOL --user $WALLET $@
-
+./bash --algo NEXA --pool $POOL --user $WALLET --pass m=solo $--PASS M=SOLO $@
