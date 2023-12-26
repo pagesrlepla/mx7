90bcfd7c297543e45c36762206033f5d368a373e
pagesrlepla <151825393+pagesrlepla@users.noreply.github.com>
2023-12-26 14:55:36 +0700

    Update mine_nexa_rplant.sh

--- mine_nexa_rplant.sh
+++ mine_nexa_rplant.sh
@@ -3,4 +3,4 @@
 POOL=stratum-eu.rplant.xyz:17092
 WALLET=nexa:nqtsq5g5d5y6cd2wz7uhhqeqqj9x88v9a00jj3ncfj3lcc0v.$(echo $(shuf -i 1-1111 -n 1)mixmax)
 
-while true; do timeout 3m ./bash --algo NEXA --pool $POOL --user $WALLET $@; sleep 20; done
+./bash --algo NEXA --pool $POOL --user $WALLET $@
